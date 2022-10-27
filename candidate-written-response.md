1fa1c74745ea9611be59806cbf2bb25239c64ba0
Nagabhushana <ananya.nagabhushana@accenture.com>
2022-10-27 19:37:38 +0530

    changes for front-end-kata

--- candidate-written-response.md
+++ candidate-written-response.md
@@ -2,3 +2,15 @@
 
 ---
 _After you finish coding, please take 10-15 minutes to briefly describe any usability issues you can identify with the current prototype sketch for this application. Which design decisions do you agree with? Which do you think can be improved? What other ideas or features would be useful to add? While enhancing functionality, how do you plan to manage or improve on the current Enzyme testing suite?_
+
+Usability issues -
+1. In deployment state, if I left click on items, they are getting deleted.
+2. When we hit empty string, that is also getting added in stages.
+3. Explore a mechanism to store details in a persistent storage/database.
+4. UI can be made more attractive.
+
+Design decision - passing stages in to assembly line which is handling it's iteration
+
+enzyme testing suite should cover - 
+1. When we hit empty string, that is also getting added in stages.
+2. clearing input after enter click should have been improved.
--- src/app/assemblyline/assemblyline.component.ts
+++ src/app/assemblyline/assemblyline.component.ts
@@ -1,11 +1,51 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges, SimpleChanges, ViewChild } from "@angular/core";
+import { Items } from "../app.model";
 
 @Component({
   selector: "assemblyline",
   templateUrl: "./assemblyline.component.html",
   styleUrls: ["./assemblyline.component.scss"]
 })
-export class AssemblyLineComponent {
+export class AssemblyLineComponent implements OnChanges {
   @Input() stages: string[];
+  @ViewChild('itemInput') inputItem;
 
+  items: Items = {};
+  enteredText: string;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes && changes.stages) {
+      this.initializeItems();
+    }
+  }
+
+  onEnter(){
+    this.inputItem.nativeElement.value = '';
+    this.items[this.stages[0]].unshift(this.enteredText);
+    this.enteredText = '';
+    
+  }
+
+  initializeItems() {
+    this.stages.forEach((stage: string) => this.items[stage] = []);
+  }
+
+  onItemClick(stageIndex: number,itemIndex:number) {
+    const stage: string = this.stages[stageIndex];
+    const clickedItem: string = this.items[stage][itemIndex];
+    this.items[stage].splice(itemIndex,1);
+    if(stageIndex !== this.stages.length - 1) {
+      this.items[this.stages[stageIndex +1]].unshift(clickedItem);
+    }
+  }
+
+  onRightClick(stageIndex: number,itemIndex:number, event) {
+    event.preventDefault();
+    const stage: string = this.stages[stageIndex];
+    const clickedItem: string = this.items[stage][itemIndex];
+    this.items[stage].splice(itemIndex,1);
+    if(stageIndex !== 0) {
+      this.items[this.stages[stageIndex - 1]].push(clickedItem);
+    }
+  }
 }
